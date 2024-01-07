326e4fc05718bf66b32a308170f9089aa6bb50a7
Prasad Nallajala <nalajalaprasad1018@gmail.com>
2024-01-07 11:27:18 +0000

    Updated

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ const initializeDb = async () => {
       driver: sqlite3.Database,
     });
 
-    app.listen(3000, () => console.log("Server started"));
+    app.listen(3001, () => console.log("Server started"));
   } catch (e) {
     console.log(`Got an error in ${e.message}`);
   }
 };
 initializeDb();
 
+app.use(express.json());
 //GET API
 
 app.get("/players/", async (request, response) => {
@@ -28,8 +29,30 @@ app.get("/players/", async (request, response) => {
     SELECT *
     FROM cricket_team`;
   const data = await db.all(query);
-  console.log(data);
+  // console.log(data);
   response.send(data);
+});
+
+app.post("/players/", async (request, response) => {
+  try {
+    const bookDetails = request.body;
+    console.log("book Details" + bookDetails);
+    const { playerName, jerseyNumber, role } = bookDetails;
+
+    const query = `
+  INSERT INTO cricket_team(playerName,jerseyNumber,role,playerId) VALUES(
+      '${playerName}',
+      '${jerseyNumber}',
+      '${role}',
+      
+  )
+  `;
+    const responseItem = await db.run(query);
+
+    response.send("Player Added to Team");
+  } catch (e) {
+    console.log(`Getting error ${e}`);
+  }
 });
 
 module.exports = app;
